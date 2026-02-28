6bde1ce876c583418ca70a9692c3361db5ba4d5f
felinesy <rubikatrinaa@gmail.com>
2026-02-28 20:27:22 +0800

    Update Veterinarian.java

--- Veterinarian.java
+++ Veterinarian.java
@@ -1,20 +1,31 @@
-public class MedicalRecord {
+public class Veterinarian implements PetCareService {
 
-    private int recordID;
-    private String diagnosis;
-    private String treatment;
-    private String checkupDate;
+    private String name;
 
-    public MedicalRecord(int recordID, String diagnosis, String treatment, String checkupDate) {
-        this.recordID = recordID;
-        this.diagnosis = diagnosis;
-        this.treatment = treatment;
-        this.checkupDate = checkupDate;
+    public Veterinarian(String name) {
+        this.name = name;
     }
 
-    public void updateRecord(String diagnosis, String treatment) {
-        this.diagnosis = diagnosis;
-        this.treatment = treatment;
-        System.out.println("Medical record updated.");
+    public void treatPet(Animal animal) {
+        System.out.println(name + " is treating the pet.");
+    }
+
+    public void updateRecord(MedicalRecord record) {
+        System.out.println("Updating medical record...");
+    }
+
+    @Override
+    public void feedPet(Animal animal) {
+        System.out.println("Vet feeds the pet.");
+    }
+
+    @Override
+    public void playWithPet(Animal animal) {
+        System.out.println("Vet plays with the pet.");
+    }
+
+    @Override
+    public void groomPet(Animal animal) {
+        System.out.println("Vet grooms the pet.");
     }
 }
